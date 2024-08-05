bb8d27bf105556d0be68d1f3c85bf12c4daf30c0
omereliyahu1 <omer.eliyahu@pagaya.com>
2024-08-05 07:58:37 +0300

    add origins

--- web_api/api.py
+++ web_api/api.py
@@ -9,7 +9,7 @@ from fastapi import FastAPI, Request
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8001"],
+    allow_origins=["http://localhost:8001", "https://omereliyahu2.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
